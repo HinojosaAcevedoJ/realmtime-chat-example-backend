2ddd5b7fcb69061b8a8c7903a67c7f02f4f75bb8
HinojosaAcevedoJ <hinojosaacevedoj@gmail.com>
2023-02-17 01:59:16 -0300

    fix: minimal fixes to middlewares
    minimal fixes isMember and verifyOwnership middleware

--- src/middlewares/isMember.js
+++ src/middlewares/isMember.js
@@ -1,15 +1,21 @@
+const mongoose = require('mongoose')
 const Room = require('../validations/RoomModelSchema')
 
-async function isMember(req, res, next) {
-  const { roomId } = req.params
-  const userId = res.locals.decodedToken.sub
-  const room = await Room.findOne({ _id: roomId, users: userId })
-  if (!room) {
-    res.status(401).json({ message: 'Unauthorized' })
-    return
-  }
+const { ObjectId } = mongoose.Types
 
+async function isMember(req, res, next) {
   try {
+    const { roomId } = req.params
+    const userId = res.locals.decodedToken.sub
+    const room = await Room.findById(ObjectId(roomId))
+    if (!room) {
+      res.status(401).json({ message: 'Unauthorized' })
+      return
+    }
+    if (!room.users.includes(userId)) {
+      res.status(401).json({ message: 'Unauthorized' })
+      return
+    }
     await next()
   } catch (err) {
     res.status(500).json({ message: err.message })
--- src/middlewares/verifyOwnership.js
+++ src/middlewares/verifyOwnership.js
@@ -1,15 +1,23 @@
+const mongoose = require('mongoose')
 const Room = require('../validations/RoomModelSchema')
 
-async function verifyOwnership(req, res, next) {
-  const { roomId } = req.params
-  const userId = res.locals.decodedToken.sub
-  const room = await Room.findOne({ _id: roomId, owner: userId })
-  if (!room) {
-    res.status(401).json({ message: 'Unauthorized' })
-    return
-  }
+const { ObjectId } = mongoose.Types
 
+async function verifyOwnership(req, res, next) {
   try {
+    const { roomId } = req.params
+    const userId = res.locals.decodedToken.sub
+    const room = await Room.findById(ObjectId(roomId))
+    if (!room) {
+      res.status(404).json({ message: 'Sala no encontrada' })
+      return
+    }
+
+    if (room.owner.toString() !== userId) {
+      res.status(403).json({ message: 'No tienes permiso para realizar esta acción' })
+      return
+    }
+
     await next()
   } catch (err) {
     res.status(500).json({ message: err.message })
