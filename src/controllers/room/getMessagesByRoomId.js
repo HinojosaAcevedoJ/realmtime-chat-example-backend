fbefd6f38cfcd5268053fd3c5d06bec0bc161f30
HinojosaAcevedoJ <hinojosaacevedoj@gmail.com>
2023-02-17 20:39:58 -0300

    fix: minimal fixes

--- src/controllers/room/getMessagesByRoomId.js
+++ src/controllers/room/getMessagesByRoomId.js
@@ -8,10 +8,12 @@ const getMessagesByRoomId = async (req, res) => {
   const { roomId } = req.params
   if (!ObjectId.isValid(roomId)) {
     res.status(404).json({ message: 'Sala no encontrada' })
+    return
   }
   const room = await Room.findById(roomId)
   if (!room) {
     res.status(404).json({ message: 'Sala no encontrada' })
+    return
   }
   const data = await Message.find({ room: roomId }).populate('user')
   res.send(data)
